85c4de198b2ea1fbc39f32f7c915045dad53997f
naliev <sms290@yandex.ru>
2022-10-30 20:53:50 +0300

    User class initialization

---
+++ src/main/java/com/naliev/restaurantrating/model/Role.java
@@ -0,0 +1,6 @@
+package com.naliev.restaurantrating.model;
+
+public enum Role {
+    ADMIN,
+    USER
+}
--- src/main/java/com/naliev/restaurantrating/model/User.java
+++ src/main/java/com/naliev/restaurantrating/model/User.java
@@ -1,10 +1,83 @@
 package com.naliev.restaurantrating.model;
 
+import org.springframework.util.CollectionUtils;
+
+import java.util.*;
+
 public class User extends AbstractNamedEntity {
+    private String email;
+    private String password;
+    private Set<Role> roles;
+    private boolean enabled = true;
+    private Date registered = new Date();
+
     public User() {
     }
 
-    public User(Integer id, String name) {
+    public User(User u) {
+        this(u.id, u.name, u.email, u.password, u.enabled, u.registered, u.roles);
+    }
+
+    public User(Integer id, String name, String email, String password, Role... roles) {
+        this(id, name, email, password, true, new Date(), Arrays.asList((roles)));
+    }
+
+    public User(Integer id, String name, String email, String password, boolean enabled, Date registered, Collection<Role> roles) {
         super(id, name);
+        this.email = email;
+        this.password = password;
+        this.enabled = enabled;
+        this.registered = registered;
+        setRoles(roles);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Date registered) {
+        this.registered = registered;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = CollectionUtils.isEmpty(roles) ? EnumSet.noneOf(Role.class) : EnumSet.copyOf(roles);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id +
+                ", email=" + email +
+                ", name=" + name +
+                ", enabled=" + enabled +
+                ", roles=" + roles +
+                '}';
     }
 }
