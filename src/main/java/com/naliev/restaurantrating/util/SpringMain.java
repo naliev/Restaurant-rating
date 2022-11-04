69a1b65b95d2210303799ca1132cac66e909ba4d
naliev <sms290@yandex.ru>
2022-11-04 20:57:31 +0300

    Minor update to spring main test

--- src/main/java/com/naliev/restaurantrating/util/SpringMain.java
+++ src/main/java/com/naliev/restaurantrating/util/SpringMain.java
@@ -1,8 +1,9 @@
 package com.naliev.restaurantrating.util;
 
-import com.naliev.restaurantrating.controller.RestaurantController;
-import com.naliev.restaurantrating.controller.UserController;
-import com.naliev.restaurantrating.controller.VoteController;
+import com.naliev.restaurantrating.web.SecurityUtil;
+import com.naliev.restaurantrating.web.restaurant.RestaurantRestController;
+import com.naliev.restaurantrating.web.user.UserRestController;
+import com.naliev.restaurantrating.web.vote.VoteRestController;
 import com.naliev.restaurantrating.model.Restaurant;
 import com.naliev.restaurantrating.model.Role;
 import com.naliev.restaurantrating.model.User;
@@ -15,20 +16,19 @@ import java.util.Arrays;
 
 public class SpringMain {
     public static void main(String[] args) {
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring.xml")) {
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            UserController userController = appCtx.getBean(UserController.class);
-            RestaurantController restaurantController = appCtx.getBean(RestaurantController.class);
-            VoteController voteController = appCtx.getBean(VoteController.class);
+            UserRestController userRestController = appCtx.getBean(UserRestController.class);
+            RestaurantRestController restaurantRestController = appCtx.getBean(RestaurantRestController.class);
+            VoteRestController voteRestController = appCtx.getBean(VoteRestController.class);
 
-            User u = userController.create(new User(null, "userName", "email4@mail.ru", "password", Role.ADMIN));
-            System.out.println();
+            User u = userRestController.create(new User(null, "userName", "email4@mail.ru", "password", Role.ADMIN));
+            SecurityUtil.setAuthUserId(u.getId());
+            Restaurant rProxy = restaurantRestController.create(new Restaurant(null, "Created restaurant"));
 
-            Restaurant r = restaurantController.create(new Restaurant(null, "Created restaurant"), u.getId());
-
-            Vote v = new Vote(null, u, LocalDateTime.now(), r);
-            Vote vProxy = voteController.create(v, v.getUser().getId(), v.getRestaurant().getId());
-            System.out.println(voteController.getUserVotes(u.getId()));
+            Vote v = new Vote(null, u, LocalDateTime.now(), rProxy);
+            Vote vProxy = voteRestController.create(v, v.getRestaurant().getId());
+            System.out.println(voteRestController.getUserVotes(u.getId()));
         }
     }
 }
