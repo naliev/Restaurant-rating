d1cf38e20cdf2c76183da2a3f0a8bc402edaa26c
naliev <sms290@yandex.ru>
2022-11-04 20:42:19 +0300

    Add util security class, for now it will be just giving me embedded user id

---
+++ src/main/java/com/naliev/restaurantrating/web/SecurityUtil.java
@@ -0,0 +1,18 @@
+package com.naliev.restaurantrating.web;
+
+import com.naliev.restaurantrating.model.AbstractBaseEntity;
+
+public class SecurityUtil {
+    private static int id = AbstractBaseEntity.START_SEQ;
+
+    private SecurityUtil() {
+    }
+
+    public static int authUserId() {
+        return id;
+    }
+
+    public static void setAuthUserId(int id) {
+        SecurityUtil.id = id;
+    }
+}
