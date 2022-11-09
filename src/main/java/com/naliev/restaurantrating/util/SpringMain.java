fad64b40e5188cfefc695a2b3da4e6b620792ea0
naliev <sms290@yandex.ru>
2022-11-09 22:07:59 +0300

    Fix spring main

--- src/main/java/com/naliev/restaurantrating/util/SpringMain.java
+++ src/main/java/com/naliev/restaurantrating/util/SpringMain.java
@@ -1,17 +1,16 @@
 package com.naliev.restaurantrating.util;
 
-import com.naliev.restaurantrating.web.SecurityUtil;
-import com.naliev.restaurantrating.web.restaurant.RestaurantRestController;
-import com.naliev.restaurantrating.web.user.UserRestController;
-import com.naliev.restaurantrating.web.vote.VoteRestController;
 import com.naliev.restaurantrating.model.Restaurant;
 import com.naliev.restaurantrating.model.Role;
 import com.naliev.restaurantrating.model.User;
 import com.naliev.restaurantrating.model.Vote;
+import com.naliev.restaurantrating.web.SecurityUtil;
+import com.naliev.restaurantrating.web.restaurant.RestaurantRestController;
+import com.naliev.restaurantrating.web.user.UserRestController;
+import com.naliev.restaurantrating.web.vote.VoteRestController;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 
 public class SpringMain {
@@ -25,9 +24,7 @@ public class SpringMain {
             User u = userRestController.create(new User(null, "userName", "email4@mail.ru", "password", Role.ADMIN));
             SecurityUtil.setAuthUserId(u.getId());
             Restaurant rProxy = restaurantRestController.create(new Restaurant(null, "Created restaurant"));
-
-            Vote v = new Vote(null, u, LocalDateTime.now(), rProxy);
-            Vote vProxy = voteRestController.create(v, v.getRestaurant().getId());
+            Vote vProxy = voteRestController.create(u.getId(), rProxy.getId());
             System.out.println(voteRestController.getUserVotes(u.getId()));
         }
     }
