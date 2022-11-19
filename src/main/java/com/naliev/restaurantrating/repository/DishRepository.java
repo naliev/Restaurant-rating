4fccf4c096657dcedc2bebb930ba17fb2d1c09e1
naliev <sms290@yandex.ru>
2022-11-19 23:45:49 +0300

    Initialize dish repo

--- src/main/java/com/naliev/restaurantrating/repository/DishRepository.java
+++ src/main/java/com/naliev/restaurantrating/repository/DishRepository.java
@@ -1,7 +1,19 @@
 package com.naliev.restaurantrating.repository;
 
+import com.naliev.restaurantrating.model.Dish;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 @Transactional(readOnly = true)
-public interface DishRepository {
+public interface DishRepository extends JpaRepository<Dish, Integer> {
+
+    @Query("SELECT d FROM Dish d WHERE d.id=?1 AND d.restaurant.id=?2")
+    Optional<Dish> get(int id, int restaurantId);
+
+    @Query("SELECT d FROM Dish d WHERE d.restaurant.id=?1 ORDER BY d.name")
+    List<Dish> getAll(int restaurantId);
 }
